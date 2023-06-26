f49db4d5adc9b6e64a0d88c6803d84a90260a3c5
Oğuzhan Küçükyamaç <okucukyamac@gmail.com>
2023-06-26 22:29:29 +0300

    Add project files.

---
+++ Repositories/EFCore/RepositoryBase.cs
@@ -0,0 +1,44 @@
+﻿using Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.EFCore
+{
+    public class RepositoryBase<T> : IRepositoryBase<T>
+        where T : class
+    {
+        protected readonly RepositoryContext _context;
+        public RepositoryBase(RepositoryContext context)
+        {
+            _context = context;
+        }
+        public void Create(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> GetAll(bool trackChanges, Expression<Func<T, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WebApi/Controllers/BooksController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Repositories.EFCore;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly RepositoryContext _context;
+
+        public BooksController(RepositoryContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllBooks()
+        {
+            var books = _context.Books.ToList();
+            return Ok(books);
+        }
+
+    }
+}
